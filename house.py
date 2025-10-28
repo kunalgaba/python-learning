865f8805f61212917fe91dcbd375931e76f4135c
Kunal Gaba <kunalgaba@gmail.com>
2025-10-28 08:13:12 +0000

    Conditons updated code

---
+++ house.py
@@ -0,0 +1,43 @@
+name = input("What's your name? ")
+'''
+if name == "Harry":
+    print("Gryffindor")
+elif name == "Hermione":
+    print("Gryffindor")
+elif name == "Ron":
+    print("Gryffindor")
+elif name == "Draco":
+    print("Slytherin")
+else:
+    print("Who?")
+'''
+
+'''
+if name == "Harry" or name == "Hermione" or name == "Ron":
+    print("Gryffindor")
+elif name == "Draco":
+    print("Slytherin")
+else:
+    print("Who?")
+'''
+'''
+match name:
+    case "Harry":
+        print("Gryffindor")
+    case "Hermoine":
+        print("Gryffindor")
+    case "Ron":
+        print("Gryffindor")
+    case "Draco":
+        print("Slytherin")
+    case _:
+        print("Who?")
+'''
+    
+match name:
+    case "Harry" | "Hermoine" | "Ron":
+        print("Gryffindor")
+    case "Draco":
+        print("Slytherin")
+    case _:
+        print("Who?")
