05e94502f464e5c28f1270aea103fa217c0b514c
Kunal Gaba <kunalgaba@gmail.com>
2025-10-28 07:54:00 +0000

    conditions code

---
+++ compare.py
@@ -0,0 +1,21 @@
+x = int(input("Whats x?"))
+y = int(input("Whats y?"))
+
+if x < y:
+    print("x is less than y")
+
+elif x > y:
+    print("x is greater than y")
+
+else:
+    print("x is equal to y")
+
+if x > y or x < y:
+    print("x is not equal to y")
+else:
+    print("x is equal to y")
+    
+if x!=y:
+      print("x is not equal to y")
+else:
+    print("x is equal to y")
---
+++ parity.py
@@ -0,0 +1,5 @@
+x = int(input("Whats' x? "))
+if x % 2 ==0:
+    print("Number is even")
+else:
+    print("Number is odd")
